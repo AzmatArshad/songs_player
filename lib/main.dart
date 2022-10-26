2e3e79b2677536415d8518584c6170f36caef41e
Azmat <azmatbhatti99@gmail.com>
2022-10-26 11:27:41 -0700

    MY music_player

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:music_player/Screens/home.dart';
+import 'package:music_player/provider/gettingMusic.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers:[
+        ChangeNotifierProvider(create: (ctx)=> GettingMusic(),),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const HomeScreen(),
+      ),
+    );
+  }
+}
